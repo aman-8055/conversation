b3e5635d57b84413bbe4385fd7c649d173534ec1
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-30 13:11:39 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dialogpt-chatbot',
+    version='1.0',
+    install_requires=[
+        'streamlit',
+        'torch',
+        'transformers'
+    ],
+)
